f3bf53f89ec76801235219e086db1cb81097fcf8
Ashish <ashishonline864@gmail.com>
2023-03-19 11:24:45 +0530

    Check linter

---
+++ GithubActions/src/main/java/org/ashish/Sample.java
@@ -0,0 +1,10 @@
+package org.ashish;
+
+public class Sample {
+    public static void main(String[] args) {
+        int ip=0;System.out.println("Hello World");
+        for (int i=0;i<10;i++){
+            System.out.println(i);
+        }
+    }
+}
