b81ab0b07f9ca1f4c55276b1671b07a354408789
Ashish <ashishonline864@gmail.com>
2023-03-19 10:12:07 +0530

    BFS Algorithm

---
+++ GithubActions/src/main/java/org/ashish/Edge.java
@@ -0,0 +1,10 @@
+package org.ashish;
+
+public class Edge {
+    int src;
+    int des;
+    public Edge(int src,int des) {
+        this.src=src;
+        this.des=des;
+    }
+}
---
+++ GithubActions/src/main/java/org/ashish/Main.java
@@ -0,0 +1,16 @@
+package org.ashish;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        /**
+         *  Breadth First Search (BFS)
+         *  -It is a graph traversal algorithm .
+         *  -It visits all the vertices of a graph in breadth-first order.
+         *  -It starts at a given vertex and explores the neighbouring vertices
+         *  before moving to the next level of vertices.
+         */
+
+
+    }
+}
